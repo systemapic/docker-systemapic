56e60d57dc83be491e49e66b3d5392ebc8723962
knutole <knutolesjoli@gmail.com>
2015-07-22 18:28:27 +0200

    new compose: only redis, updated docker versons

--- build/wu/run_dev_mode.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --name wu --rm --link rkue:rkue --link rtoken:rtoken --link mongo:mongo -v /data:/data -v /var/www/wu:/var/www/wu -v /root/.ssh:/root/.ssh -p 3001 -it devv/wu bash
