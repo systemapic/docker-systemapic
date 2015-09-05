fda2d10f7c248679be812d08096efedfc901acfc
knutole <knutolesjoli@gmail.com>
2015-09-05 16:45:33 +0200

    added redis:stats, cleaned up

--- build/grind/run_dev_mode.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --name vile --rm --link rkue:rkue --link rtoken:rtoken --link mongo:mongo -v /data:/data -v /var/www/vile:/var/www/vile -v /root/.ssh:/root/.ssh -p 3003 -it devv/wu bash
