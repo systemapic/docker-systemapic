8a4ec90e3c030b6b3df007923877453b13b719a7
knutole <knutolesjoli@gmail.com>
2016-01-08 15:47:47 +0100

    added backup to compose; different compose for dev/dev2/beta; backup scripts

--- backup/postgis/do_backup.sh
+++ backup/postgis/do_backup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-STORAGE_CONTAINER=debug_pg_backup_6
-BACKUP_PATH=/data/backup6
+OUTDIR=/backup/postgis
 
-docker build -t debug/pg_backup .
-docker create -v /data --name $STORAGE_CONTAINER systemapic/ubuntu
-docker run --link dev_postgis_1:postgis --volumes-from $STORAGE_CONTAINER -e "PGHOST=postgis" -it debug/pg_backup /tmp/backup_databases.sh $BACKUP_PATH
+if test -e "${OUTDIR}"; then
+	echo "Output dir already exist, will delete it!" >&2
+	
+	rm -rf $OUTDIR
+fi
+
+sh /tmp/backup_databases.sh $OUTDIR
