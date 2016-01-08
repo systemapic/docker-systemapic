a29bb2e3ffe64493b0a146f178dc6074771bf4dc
Sandro Santilli <strk@keybit.net>
2016-01-08 16:37:59 +0100

    Fix manual backup run

--- backup/postgis/backup/deprecated.run.sh
+++ backup/postgis/backup/deprecated.run.sh
@@ -4,5 +4,8 @@ STORAGE_CONTAINER=postgis_backup_store
 BACKUP_PATH=/backup/postgis
 
 docker build -t debug/pg_backup .
-docker create -v /data --name $STORAGE_CONTAINER systemapic/ubuntu
-docker run --link dev_postgis_1:postgis --volumes-from $STORAGE_CONTAINER -e "PGHOST=postgis" -it debug/pg_backup /tmp/backup_databases.sh $BACKUP_PATH
+#docker create -v /data --name $STORAGE_CONTAINER systemapic/ubuntu
+docker run --rm \
+	--link dev_postgis_1:postgis --volumes-from $STORAGE_CONTAINER \
+	systemapic/backup:postgis \
+	/tmp/do_backup.sh 
