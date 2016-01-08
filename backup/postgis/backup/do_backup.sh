43238651b8585d9c6f899ad91dce3dbf00637c8c
Sandro Santilli <strk@keybit.net>
2016-01-08 16:13:58 +0100

    Encode day-of-week in output dir name, drop a -latest symlink

--- backup/postgis/backup/do_backup.sh
+++ backup/postgis/backup/do_backup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-OUTDIR=/backup/postgis
+BASEOUTDIR=/backup/postgis/
+BACKUPNAME=postgis-backup-`date +%w` # use day of week
+OUTDIR=${BASEOUTDIR}/${BACKUPNAME}
+
+mkdir -p "${BASEOUTDIR}" || exit 1
 
 if test -e "${OUTDIR}"; then
-	echo "Output dir already exist, will delete it!" >&2
-	
-	rm -rf $OUTDIR
+	echo "NOTICE: output dir already exist, will delete it!"
+  rm -r $OUTDIR
 fi
 
 sh /tmp/backup_databases.sh $OUTDIR
+cd ${BASEOUTDIR}
+rm -f postgis-backup-last
+ln -s ${BACKUPNAME} postgis-backup-last
