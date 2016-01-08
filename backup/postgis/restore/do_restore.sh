e8891604c73720cbfcc4bb789ad887a34f3e112a
Sandro Santilli <strk@keybit.net>
2016-01-08 15:44:07 +0100

    Take store names and backup path as parameters

--- backup/postgis/restore/do_restore.sh
+++ backup/postgis/restore/do_restore.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-BACKUP_STORAGE_CONTAINER=postgis_backup_store
-BACKUP_PATH=/backup/postgis/postgis_store_dev2-dbdumps-20160107
 
-PGDATA_RESTORE_CONTAINER=postgis_store_dev2_restore
+if test -z "$3"; then
+	echo "Usage $0 <backup_store_name> <backup_store_path> <pgdata_store_name>" >&2
+	exit 1
+fi
+BACKUP_STORAGE_CONTAINER="$1"
+BACKUP_PATH="$2"
+PGDATA_RESTORE_CONTAINER="$3"
 
 # Create storage only if needed ?
-docker rm $PGDATA_RESTORE_CONTAINER
-docker create -v /var/lib/postgresql --name $PGDATA_RESTORE_CONTAINER systemapic/ubuntu
+#docker rm $PGDATA_RESTORE_CONTAINER
+docker create -v /var/lib/postgresql \
+	--name $PGDATA_RESTORE_CONTAINER \
+	systemapic/ubuntu || exit 1
 
 docker build -t debug/pg_restore .
 docker run --rm \
