a91f92bb3f62f90adc2117bdc0572de52c4697ac
Sandro Santilli <strk@keybit.net>
2016-01-15 15:22:16 +0100

    Report what's been built

--- build/postgis/build.sh
+++ build/postgis/build.sh
@@ -5,4 +5,11 @@ PGVER=9.3
 test -n "$1" && PGVER="$1"
 
 PGVER_SHORT=`echo ${PGVER} | tr -d .`
-docker build --build-arg PGVER=${PGVER} -t systemapic/postgis:${PGVER_SHORT}-21 .
+NAME="systemapic/postgis"
+TAG="${PGVER_SHORT}-21"
+FULLNAME="${NAME}:${TAG}"
+docker build --build-arg PGVER=${PGVER} -t ${FULLNAME} . || exit 1
+
+echo "Image ${FULLNAME} built"
+
+
