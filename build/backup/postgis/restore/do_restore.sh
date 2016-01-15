06ce5614054a8d5398cfc71d3f8fa5f3c6a0705d
Sandro Santilli <strk@keybit.net>
2016-01-15 17:16:43 +0100

    Refactor do_restore.sh to use new upgrade support in postgis images
    Will create the target postgresql store and run the target
    postgis version by passing SYSTEMAPIC_RESTORE_POSTGIS_FROM
    pointing to a mounted backup volume.
    
    Restore docker image is obsoleted by this commit.

--- build/backup/postgis/restore/do_restore.sh
+++ build/backup/postgis/restore/do_restore.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
+# Example:
+# ./do_restore.sh postgis_backup_store /backup/postgis/postgis-backup-last postgresql94_store_dev2 systemapic/postgis:94-21
 
 if test -z "$3"; then
-	echo "Usage $0 <backup_store_name> <backup_store_path> <pgdata_store_name>" >&2
+	echo "Usage $0 <backup_store> <backup_path> <pgdata_store> <postgis_image>" >&2
 	exit 1
 fi
 BACKUP_STORAGE_CONTAINER="$1"
 BACKUP_PATH="$2"
 PGDATA_RESTORE_CONTAINER="$3"
+POSTGIS_IMAGE="$4"
 
 # Create storage only if needed ?
 #docker rm $PGDATA_RESTORE_CONTAINER
+
+echo "--- Creating pgdata volume ${PGDATA_RESTORE_CONTAINER}"
 docker create -v /var/lib/postgresql \
-	--name $PGDATA_RESTORE_CONTAINER \
+	--name ${PGDATA_RESTORE_CONTAINER} \
 	systemapic/ubuntu || exit 1
 
-docker build -t debug/pg_restore .
-docker run --rm \
-	--volumes-from $BACKUP_STORAGE_CONTAINER \
-	--volumes-from $PGDATA_RESTORE_CONTAINER \
-	-e "PGHOST=localhost" \
-	-it debug/pg_restore \
-	./restore_databases.sh $BACKUP_PATH
+container_name=postgis_restore_$$
 
-echo "docker run returned $?"
+echo "--- Running ${POSTGIS_IMAGE} with name ${container_name} and volume ${PGDATA_RESTORE_CONTAINER}"
+docker run --rm --name ${container_name} \
+	--volumes-from ${BACKUP_STORAGE_CONTAINER} \
+	--volumes-from ${PGDATA_RESTORE_CONTAINER} \
+	-e "PGHOST=localhost" \
+	-e "SYSTEMAPIC_RESTORE_POSTGIS_FROM=${BACKUP_PATH}" \
+	${POSTGIS_IMAGE} || exit 1
+
+
 echo "Restored PGDATA should now be in ${PGDATA_RESTORE_CONTAINER}"
 
 #--volume /root:/root_host \
