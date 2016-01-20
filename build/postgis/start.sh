dfd1a397f47a1834c8cf8c651b9d7867cde5e09d
knutole <knutolesjoli@gmail.com>
2016-01-20 19:25:09 +0100

    getting restore to work

--- build/postgis/start.sh
+++ build/postgis/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-test -n "$1" || {
-  echo "Usage: $0 <pg_version>"
-  echo "  pg_version is in Major.Minor format"
-  exit 1
-} >&2
+# test -n "$1" || {
+#   echo "Usage: $0 <pg_version>"
+#   echo "  pg_version is in Major.Minor format"
+#   exit 1
+# } >&2
 
 PGVER="$1"
-
+PGVER=9.4
 echo "Starting PostgreSQL ${PGVER}"
 
 DATADIR="/var/lib/postgresql/${PGVER}/main"
