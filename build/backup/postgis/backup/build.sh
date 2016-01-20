68e70f84e7b2920ad3623067728d1165c4415a70
knutole <knutolesjoli@gmail.com>
2016-01-20 21:24:20 +0100

    init script for mongo AUTH

--- build/backup/postgis/backup/build.sh
+++ build/backup/postgis/backup/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # name of storage
-STORAGE=postgis_backup_store
+STORAGE=store_dev_postgis_backup
 
 # create storage if not exists
 RUNNING=$(docker inspect --format="{{ .Name }}" $STORAGE 2> /dev/null)
 if [ "$RUNNING" != "/$STORAGE" ]; then
+	echo "Creating $STORAGE volume..."
 	docker create -v /backup/postgis --name $STORAGE systemapic/ubuntu
 fi
 
 # build
-docker build -t systemapic/backup:postgis .
+# docker build -t systemapic/backup:postgis .
+docker build -t systemapic/postgis:backup .
