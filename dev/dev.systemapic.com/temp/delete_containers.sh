4cde796794c2226f84791fff09df2fdaefae73df
knutole <knutolesjoli@gmail.com>
2016-01-20 19:25:09 +0100

    getting restore to work

--- dev/dev.systemapic.com/temp/delete_containers.sh
+++ dev/dev.systemapic.com/temp/delete_containers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker rm devsystemapiccom_nginx_1
-docker rm devsystemapiccom_pile_1
-docker rm devsystemapiccom_wu_1
-docker rm devsystemapiccom_redislayers_1
-docker rm devsystemapiccom_redisstats_1
-docker rm devsystemapiccom_redistemp_1
-docker rm devsystemapiccom_mongo_1
-docker rm devsystemapiccom_postgis_1
+docker rm temp_nginx_1
+docker rm temp_pile_1
+docker rm temp_wu_1
+docker rm temp_redislayers_1
+docker rm temp_redisstats_1
+docker rm temp_redistemp_1
+docker rm temp_mongo_1
+docker rm temp_postgis_1
