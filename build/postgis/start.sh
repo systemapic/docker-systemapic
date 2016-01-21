59a7f636a549c796e5acdc29701837b37e5e2629
Sandro Santilli <strk@keybit.net>
2016-01-21 17:24:10 +0100

    Refactor postgis image to check/enforce user/password and database
    The Dockerfile itself will not create any database, nor set any
    systemapic-specific connection env variables.
    
    The logic, moved down to "start.sh" is as follows:
    
     - USERNAME, PASS, DBNAME are given
       (currently hard-coded but later to be possibly passed as parameter
       or via config file)
    
     - The script ensures USERNAME exists as a user, has the given PASS
       password and owns all non-system databases
    
     - The script ensures the given DBNAME exists
    
    This has been tested with a new store container, an existing
    one with missing user, an existing one with missing DBNAME,
    an existing one with bogus database ownership.
    
    See #24

--- build/postgis/start.sh
+++ build/postgis/start.sh
@@ -7,15 +7,18 @@
 # } >&2
 
 PGVER="$1"
-echo "Starting PostgreSQL ${PGVER}"
 
 DATADIR="/var/lib/postgresql/${PGVER}/main"
 CONF="/etc/postgresql/${PGVER}/main/postgresql.conf"
-POSTGRES="/usr/lib/postgresql/${PGVER}/bin/postgres"
-INITDB="/usr/lib/postgresql/${PGVER}/bin/initdb"
+BINDIR="/usr/lib/postgresql/${PGVER}/bin/"
+POSTGRES="${BINDIR}/postgres"
+INITDB="${BINDIR}/initdb"
+
+# TODO: take these as environment variable ?
 USERNAME="docker"
 PASS="docker"
 DBNAME="systemapic"
+
 EXISTING_CLUSTER=yes
 
 # test if DATADIR is existent
@@ -27,39 +30,91 @@ fi
 # set ownership of datadir
 chown -R postgres ${DATADIR} || exit 1
 
-# test if DATADIR has content
+# test if DATADIR has content, or call initdb
 if [ ! "$(ls -A $DATADIR)" ]; then
   EXISTING_CLUSTER=no
-  echo "Initializing Postgres Database at $DATADIR"
-  # chown -R postgres $DATADIR
+  echo "Initializing PostgreSQL cluster at $DATADIR"
   su postgres sh -c "$INITDB $DATADIR" || exit 1
-
-  su postgres sh -c "$POSTGRES --single -D $DATADIR -c config_file=$CONF"<<EOF || exit 1
-CREATE USER $USERNAME WITH SUPERUSER PASSWORD '$PASS';
-CREATE DATABASE $DBNAME;
-EOF
 fi
 
 trap "echo \"Sending SIGTERM to postgres\"; killall -s SIGTERM postgres" SIGTERM
 
+echo "--- Starting PostgreSQL ${PGVER}"
 su postgres sh -c "$POSTGRES -D $DATADIR -c config_file=$CONF" &
 pgsqlpid=$!
+
+export PGDATABASE=template1
+export PGUSER=postgres
+export PGHOST=localhost
+
+echo "Waiting for postgres start for restoring databases"
+PSQL="psql -X"
+while :; do
+        sleep 5;
+        sudo -u postgres ${PSQL} -c "select version();" && break;
+done
 
 if test -n "${SYSTEMAPIC_RESTORE_POSTGIS_FROM}"; then
   if test "${EXISTING_CLUSTER}" = 'yes'; then
     echo "SYSTEMAPIC_RESTORE_POSTGIS_FROM is set but DATADIR ${DATADIR} is pre-existing" >&2
     exit 1
   else
-    echo "Waiting for postgres start for restoring databases"
-    export PGDATABASE=template1
-    PSQL="psql -X"
-    while :; do
-            sleep 5;
-            ${PSQL} -c "select version();" && break;
-    done
     echo "Attempting restore from ${SYSTEMAPIC_RESTORE_POSTGIS_FROM}"
     `dirname $0`/restore_databases.sh ${SYSTEMAPIC_RESTORE_POSTGIS_FROM}
   fi
-else
-  wait $pgsqlpid
+  exit 0
 fi
+
+# ensure USERNAME exists, owns all databases (except system ones)
+# and has the given password
+cat<<EOF | sudo -u postgres ${PSQL} -tA template1
+DO \$\$
+DECLARE
+  rec RECORD;
+BEGIN
+
+  -- Ensure user exists and has given password
+  IF NOT EXISTS (
+    SELECT 1 FROM pg_catalog.pg_user WHERE usename = '${USERNAME}'
+    )
+  THEN
+    -- Create user
+    RAISE WARNING 'Creating missing "${USERNAME}" user';
+    CREATE USER "${USERNAME}" WITH SUPERUSER PASSWORD '${PASS}';
+  ELSE
+    -- Set correct password and "superuser" flag
+    RAISE NOTICE 'Ensuring "${USERNAME}" password';
+    ALTER USER "${USERNAME}" SUPERUSER PASSWORD '${PASS}';
+  END IF;
+
+  -- Ensure user owns all databases
+  FOR rec IN
+    SELECT d.datname, u.usename
+    FROM pg_catalog.pg_user u, pg_database d
+    WHERE u.usesysid = d.datdba
+      AND u.usename != '${USERNAME}'
+      AND d.datname NOT IN ( 'template1','template0','postgres' )
+  LOOP
+    RAISE WARNING 'Changing ownership of database % from "%" to "${USERNAME}"', rec.datname, rec.usename;
+    EXECUTE 'ALTER DATABASE ' || quote_ident(rec.datname)
+      || ' OWNER TO "${USERNAME}"';
+  END LOOP;
+
+END;
+\$\$
+LANGUAGE 'plpgsql';
+EOF
+
+# Ensure DBNAME exists or create one
+# (cannot be done in the DO block above)
+exists=`sudo -u postgres ${PSQL} -tA -c "\
+SELECT 'yes' FROM pg_catalog.pg_database WHERE datname = '${DBNAME}' \
+" template1`
+if test "$exists" != "yes"; then
+  echo "WARNING: Creating missing \"${DBNAME}\" database";
+  sudo -u postgres createdb -O "${USERNAME}" "${DBNAME}"
+fi
+
+# TODO: drop unkonwn users ?
+
+wait $pgsqlpid
