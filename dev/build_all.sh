e8359ca277124c02f6d527ef6dfb755365292683
Sandro Santilli <strk@keybit.net>
2016-01-28 12:45:23 +0100

    Ask for target domain when missing

--- dev/build_all.sh
+++ dev/build_all.sh
@@ -2,6 +2,20 @@
 
 DIRS="nginx mongo redis/kue redis/layers redis/stats postgis backup/postgis/backup pile wu"
 
+if test -z "$SYSTEMAPIC_DOMAIN"; then
+  DEFAULT_SYSTEMAPIC_DOMAIN=dev.systemapic.com
+  echo -n "Enter target domain [${DEFAULT_SYSTEMAPIC_DOMAIN}]:"
+  read SYSTEMAPIC_DOMAIN
+  if test -z "$SYSTEMAPIC_DOMAIN"; then
+    SYSTEMAPIC_DOMAIN=${DEFAULT_SYSTEMAPIC_DOMAIN}
+  fi
+  export SYSTEMAPIC_DOMAIN
+fi
+
+echo "--------------------------------------------------------------"
+echo "Building all dockers for target domain ${SYSTEMAPIC_DOMAIN}"
+echo "--------------------------------------------------------------"
+
 cd ../build
 for dir in $DIRS; do
   echo "Building $dir from $PWD"
