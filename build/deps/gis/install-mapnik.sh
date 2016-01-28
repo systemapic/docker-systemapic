eafdf90ee5e9b55db4969d7459719393d5d1ae1c
Sandro Santilli <strk@keybit.net>
2016-01-28 14:49:55 +0100

    Separate mapnik download and install steps, install 3.0.9
    The change makes the mapnik build "stable" in that it doesn't
    depend on whatever is pushed to its "master" branch.
    
    Fixes #27

--- build/deps/gis/install-mapnik.sh
+++ build/deps/gis/install-mapnik.sh
@@ -5,11 +5,11 @@ die() {
     exit 1
 }
 
-cd /tmp/ || die
-git clone https://github.com/mapnik/mapnik  || die
-cd mapnik || die
+test -n "$1"  || { echo "Usage: $0 <source_dir>" >&2; exit 1; }
+SRCDIR=$1
+
+cd "${SRCDIR}" || die
 ./configure BOOST_INCLUDES=/opt/boost/include BOOST_LIBS=/opt/boost/lib || die
 make -j7 || die
 sudo make install || die
 
-rm /tmp/mapnik -r || die
