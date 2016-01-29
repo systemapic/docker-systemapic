b90e9dcb49d1c56c74f7b2340ecfa9996b485b3e
knutole <knutolesjoli@gmail.com>
2016-01-29 14:37:48 +0100

    incorporated @strk's checks for config file in restart.sh

--- compose/restart.sh
+++ compose/restart.sh
@@ -5,6 +5,10 @@ function abort() {
 	exit 1;
 }
 
+# check config file exists for wu and pile
+test -f /var/www/wu/config/server-config.js || abort "ERROR: A /var/www/wu/config/server-config.js file is needed for wu to run"
+test -f /var/www/pile/config/pile-config.js || abort "ERROR: A /var/www/pile/config/pile-config.js file is needed for pile to run"
+
 # check domain ENV is set
 echo "Checking host domain...$SYSTEMAPIC_DOMAIN"
 [ -z "$SYSTEMAPIC_DOMAIN" ] && abort "Restart failed! Need to set SYSTEMAPIC_DOMAIN ENV variable, eg. export SYSTEMAPIC_DOMAIN=dev.systemapic.com"
@@ -14,11 +18,12 @@ COMPOSEFILE="$SYSTEMAPIC_DOMAIN".yml
 ARR=(${SYSTEMAPIC_DOMAIN//./ })
 COMPOSENAME=${ARR[0]} 
 
+# kill, delete, start fresh, get logs
 echo -e "\e[93mKilling containers...\e[39m"
 docker-compose -f $COMPOSEFILE -p $COMPOSENAME kill
 echo -e "\e[93mDeleting containers...\e[39m"
 ./delete_containers.sh
 echo -e "\e[93mStarting containers...\e[39m"
-docker-compose -f $COMPOSEFILE -p $COMPOSENAME up -d
+docker-compose -f $COMPOSEFILE -p $COMPOSENAME up -d || abort "If missing containers, try ./create_storage_containers.sh"
 echo -e "\e[93mOpening logs...\e[39m"
 docker-compose -f $COMPOSEFILE -p $COMPOSENAME logs
