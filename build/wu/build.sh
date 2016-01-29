842095b37a2fb59324633e74729a6fbb76ace8a4
Sandro Santilli <strk@keybit.net>
2016-01-29 15:52:10 +0100

    Cleanups

--- build/wu/build.sh
+++ build/wu/build.sh
@@ -2,9 +2,10 @@
 
 BD=`dirname $0`
 
-trap "echo 'Cleaning up downloaded repos'; rm -rf $BD/wu; rm -rf $BD/public" 0
+trap "echo 'Cleaning up'; rm -rf $BD/wu" 0
 
 # docker doesn't allow ADDing paths above Dockerfile dirname
+# NOTE: it is intentional to omit the .git dir
 mkdir wu && cp -r ../../modules/wu/* wu || exit 1
 
 # stamp package.json with original time
