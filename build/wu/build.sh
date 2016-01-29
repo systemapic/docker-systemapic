1a98751b15991775991076607bcd9509ca0b57de
Sandro Santilli <strk@keybit.net>
2016-01-29 15:56:28 +0100

    Remove unneeded hack to avoid npm install step re-run
    I've tested and docker checks for file hash, not timestamp

--- build/wu/build.sh
+++ build/wu/build.sh
@@ -8,14 +8,6 @@ trap "echo 'Cleaning up'; rm -rf $BD/wu" 0
 # NOTE: it is intentional to omit the .git dir
 mkdir wu && cp -r ../../modules/wu/* wu || exit 1
 
-# stamp package.json with original time
-cd wu &&
-REV=$(git rev-list -n 1 HEAD 'package.json') \
-STAMP=$(git show --pretty=format:%ai --abbrev-commit "$REV" | head -n 1) \
-touch -d "$STAMP" package.json &&
-cd - || exit 1
-
-
 # build
 docker build -t systemapic/wu .
 
