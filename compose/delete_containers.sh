017d4cb9b6bbc80e7e9a0ca7aa81397821a8345b
knutole <knutolesjoli@gmail.com>
2016-01-29 14:15:47 +0100

    Updated compose configuration

---
+++ compose/delete_containers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function abort() {
+	echo $1
+	exit 1;
+}
+
+# check domain ENV is set
+[ -z "$SYSTEMAPIC_DOMAIN" ] && abort "Delete failed! Need to set SYSTEMAPIC_DOMAIN ENV variable, eg. export SYSTEMAPIC_DOMAIN=dev.systemapic.com"
+
+# get name
+ARR=(${SYSTEMAPIC_DOMAIN//./ })
+COMPOSENAME=${ARR[0]} 
+
+# remove stale containers
+docker rm "$COMPOSENAME"_nginx_1
+docker rm "$COMPOSENAME"_pile_1
+docker rm "$COMPOSENAME"_wu_1
+docker rm "$COMPOSENAME"_redislayers_1
+docker rm "$COMPOSENAME"_redisstats_1
+docker rm "$COMPOSENAME"_redistemp_1
+docker rm "$COMPOSENAME"_mongo_1
+docker rm "$COMPOSENAME"_postgis_1
