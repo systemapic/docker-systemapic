0874420254662393bacf1f6481938b297b419f96
knutole <knutolesjoli@gmail.com>
2016-01-30 20:16:21 +0100

    moved compose files to yml/; fixes

--- build/wu/start.sh
+++ build/wu/start.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+function abort() {
+	echo $1
+	exit 1;
+}
+
+WU_DIR=/var/www/wu
+WU_CONFIG_DIR=$WU_DIR/config
+NPM_MODULES_DIR=$WU_DIR/npm_modules
+SYSTEMAPIC_CONFIG_DIR=/systemapic/config
+
+# check config is connected
+if [ ! -d "$SYSTEMAPIC_CONFIG_DIR" ]; then
+	abort "Configuration not installed, should be at $SYSTEMAPIC_CONFIG_DIR. Quitting!"
+fi
+
 # copy config file
-cd /var/www/wu
-mkdir -p config
-cp /systemapic/config/wu-config.js config/server-config.js
+mkdir -p $WU_CONFIG_DIR
+cp $SYSTEMAPIC_CONFIG_DIR/wu-config.js $WU_CONFIG_DIR/server-config.js
+
+# ensure npm is installed
+if [ ! -d "$NPM_MODULES_DIR" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  echo "Installing node modules..."
+  npm install || abort "Failed to install node modules. Quitting!"
+fi
 
 # start server
+cd /var/www/wu
 ./server.sh
