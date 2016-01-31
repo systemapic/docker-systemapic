c802070cd0a31f9de199c54dfd59dc3d0ee3a8e7
knutole <knutolesjoli@gmail.com>
2016-01-31 18:42:40 +0100

    Refactored PostGIS build
    - put all build files in /common/ folder
    - build.sh script in root will prompt for version if not specified.
    
    - TODO: print PostGIS version in start.sh
    - TODO: add support for $LATEST in build.sh

--- build/postgis/build.sh
+++ build/postgis/build.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-PGVER=9.4
-LATEST=yes
+function build() {
+  ./common/build.sh $PGVER
+}
 
-while test -n "$1"; do
-  opt=$1
-  shift
-  echo "opt:$opt, args:$@"
-  if test "$opt" = "--latest"; then
-    LATEST="yes"
-  elif test "$opt" = "--no-latest"; then
-    LATEST="no"
-  else
-    PGVER="$opt"
-  fi
-done
+DEFAULT_PGVER=9.4
+PGVER=$1
 
-PGVER_SHORT=`echo ${PGVER} | tr -d .`
-NAME="systemapic/postgis"
-TAG="${PGVER_SHORT}-21"
-FULLNAME="${NAME}:${TAG}"
-LATESTNAME="${NAME}:latest"
-echo "Building $PGVER $FULLNAME"
-docker build --build-arg PGVER=${PGVER} -t ${FULLNAME} . || exit 1
-
-echo "Image ${FULLNAME} built"
-
-if test "$LATEST" = "yes"; then
-  docker tag -f ${FULLNAME} ${LATESTNAME}
-  echo "Image tagged as ${LATESTNAME}"
+# prompt version if not specified like `./build.sh 9.3`
+if test "$PGVER" = ""; then
+  echo "Usage: ./build.sh [PGVER] (like: ./build.sh 9.4)"
+  echo ""
+  read -e -p "Enter PostgreSQL version to build: [$DEFAULT_PGVER] " PGVER
 fi
 
+# set default PGVER
+if test "$PGVER" = ""; then PGVER=$DEFAULT_PGVER; fi
+
+# only match supported versions
+if [ "$PGVER" = "9.4" ] || [ "$PGVER" = "9.3" ]; then
+  build
+else
+  echo "Build support only for either 9.4 or 9.3. Quitting!"
+  exit 1
+fi
--- build/postgis/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d --name postgis --volumes-from postgis_store_dev systemapic/postgis:latest
