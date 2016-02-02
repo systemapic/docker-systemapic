1ce6097ddd12e15d940bf7bc70d837dc3a740096
knutole <knutole@systemapic.com>
2016-02-02 03:23:52 +0100

    fixed init_mongo script

--- build/mongo/init_mongo.js
+++ build/mongo/init_mongo.js
@@ -1,25 +1,19 @@
-var fs = require('fs');
-
-var configFile = '/systemapic/config/mongo.json';
 
 // read config
-fs.readFile(configFile, 'utf-8', function (err, json) {
-	if (err) throw err;
+var configFile = cat('/systemapic/config/mongo.json');
 
-	// parse config
-	var config = JSON.parse(json);
-	var password = config.password;
-	var user = config.user;
-	var database = config.database || 'systemapic';
+// parse config
+var config = JSON.parse(configFile);
+var password = config.password;
+var user = config.user;
+var database = config.database || 'systemapic';
 
-	// prime db for auth
-	var admin = connect('localhost:27017/admin');
-	admin.system.users.remove({})
-	admin.system.version.remove({})
-	admin.system.version.insert({ '_id' : 'authSchema', 'currentVersion' : 3 })
+// prime db for auth
+var admin = connect('localhost:27017/admin');
+admin.system.users.remove({})
+admin.system.version.remove({})
+admin.system.version.insert({ '_id' : 'authSchema', 'currentVersion' : 3 })
 
-	// add user
-	var db = connect('localhost:27017/systemapic');
-	db.createUser({user : user, pwd: password, roles : [{role : 'root', db: 'admin'}, {role : 'dbOwner', db: database}]})
-
-});
+// add user
+var db = connect('localhost:27017/systemapic');
+db.createUser({user : user, pwd: password, roles : [{role : 'root', db: 'admin'}, {role : 'dbOwner', db: database}]})
