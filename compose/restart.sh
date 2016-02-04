35bdf34a7c872d06359cd3ca38afba6222baa006
Sandro Santilli <strk@keybit.net>
2016-02-04 09:27:01 +0100

    Allow running restart.sh from an arbitrary directory
    Also enhance tips on failure

--- compose/restart.sh
+++ compose/restart.sh
@@ -16,6 +16,9 @@ echo "--------------------------------------------------------------------"
 echo "Restarting services for domain $SYSTEMAPIC_DOMAIN"
 echo "--------------------------------------------------------------------"
 
+BASEDIR=`dirname $0`
+cd $BASEDIR
+
 # get file and name (eg. dev.systemapic.com.yml and dev)
 COMPOSEFILE="yml/$SYSTEMAPIC_DOMAIN".yml
 ARR=(${SYSTEMAPIC_DOMAIN//./ })
@@ -26,6 +29,9 @@ COMPOSENAME=${ARR[0]}
 echo -e "\e[93mDeleting containers...\e[39m"
 ./delete_containers.sh
 echo -e "\e[93mStarting containers...\e[39m"
-docker-compose -f $COMPOSEFILE -p $COMPOSENAME up -d || abort "If missing containers, try ./create_storage_containers.sh"
+docker-compose -f $COMPOSEFILE -p $COMPOSENAME up -d ||
+  abort "If missing containers, try running:
+        ${BASEDIR}/create_storage_containers.sh
+        ${BASEDIR}/yml/${SYSTEMAPIC_DOMAIN}.yml"
 echo -e "\e[93mOpening logs...\e[39m"
 docker-compose -f $COMPOSEFILE -p $COMPOSENAME logs
