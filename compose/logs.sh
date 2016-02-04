60ddb293fb73dfbb5c68533f86d09b7a831ed344
Sandro Santilli <strk@keybit.net>
2016-02-04 11:36:16 +0100

    Allow passing "docker-compose logs" parameter on commandline
    This is to allow things like:
    
     compose/logs.sh wu
    
    to only read logs from the "wu" container

--- compose/logs.sh
+++ compose/logs.sh
@@ -4,4 +4,4 @@ BASEDIR=`dirname $0`
 COMPOSEFILE="${BASEDIR}/yml/$SYSTEMAPIC_DOMAIN".yml
 ARR=(${SYSTEMAPIC_DOMAIN//./ })
 COMPOSENAME=${ARR[0]} 
-docker-compose -f $COMPOSEFILE -p $COMPOSENAME logs
+docker-compose -f $COMPOSEFILE -p $COMPOSENAME logs $@
