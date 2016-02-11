953b1275da1b94f0504fbcd611e26154cbc6ac2e
Sandro Santilli <strk@keybit.net>
2016-02-11 17:52:10 +0100

    Write a simplified restore/run.sh
    Update the README.md with full explanation

---
+++ build/backup/postgis/restore/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+test -n "$1" && SYSTEMAPIC_DOMAIN=`echo "$1" | sed 's/\.yml$//'`
+
+# check SYSTEMAPIC_DOMAIN is set
+test -z "$SYSTEMAPIC_DOMAIN" &&
+  abort "Usage: $0 <domain> (or set SYSTEMAPIC_DOMAIN ENV variable, eg. export SYSTEMAPIC_DOMAIN=localhost)"
+export SYSTEMAPIC_DOMAIN
+
+BASEDIR=$(cd `dirname $0`; pwd)
+
+STORENAME=postgresql_store_${SYSTEMAPIC_DOMAIN}
+BKSTORENAME=${STORENAME}_$$
+
+docker rename ${STORENAME} ${BKSTORENAME} || exit 1
+
+echo "PostgreSQL store renamed to ${BKSTORENAME} during operations"
+
+cd ${BASEDIR} &&
+./do_restore.sh \
+  postgis_backup_store \
+  /backup/postgis/postgis-backup-last \
+  ${STORENAME} \
+  systemapic/postgis:latest &&
+echo "Database restores, you can now restart services" ||
+{
+  echo "Something went wrong, renaming back PostgreSQL store"
+  docker rename ${BKSTORENAME} ${STORENAME}
+}
