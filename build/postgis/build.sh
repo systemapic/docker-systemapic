2075990146b7a792558eca91ba5bc69344f8fd72
Sandro Santilli <strk@keybit.net>
2016-02-11 18:02:54 +0100

    Cleanup directories navigation

--- build/postgis/build.sh
+++ build/postgis/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-function build() {
-  ./common/build.sh $PGVER || abort "Building PostgreSQL failed!"
-}
-
 DEFAULT_PGVER=9.3
 
 PGVER=$DEFAULT_PGVER
 test -n "$1" && PGVER=$1
 
-build
+cd common
+./build.sh $PGVER || abort "Building PostgreSQL failed!"
