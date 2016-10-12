c0ae603dbc4535493000cb1e61951f54a4e0fcce
knutole <knutolesjoli@gmail.com>
2016-10-12 15:46:19 +0200

    Docker Compose update script; subs

--- scripts/update_docker_compose.sh
+++ scripts/update_docker_compose.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo Updating Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
+# get latest version
+LATEST=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest)
+VERSION=$(echo $LATEST | sed 's:.*/::')
+
+# download
+curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose version
