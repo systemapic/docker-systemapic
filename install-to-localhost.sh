09f4d924cb6e43aad4d0f5a8d8626867ac42afbc
knutole <knutolesjoli@gmail.com>
2016-10-22 21:34:37 +0200

    Install script for CI - draft = https://github.com/mapic/engine/issues/3

---
+++ install-to-localhost.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get working dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# init dockerize submodules
+cd $DIR
+git submodule init
+git submodule update --recursive --remote
+git submodule foreach --recursive git checkout master
+
+# init mile submodules
+cd $DIR/modules/mile
+git submodule init
+git submodule update --recursive --remote
+
+# init engine submodules
+cd $DIR/modules/engine
+git submodule init
+git submodule update --recursive --remote
+
