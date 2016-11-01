56b1664dd02bc7b64d2038a2c110038a3b3191c2
knutole <knutole@mapic.io>
2016-11-01 17:54:54 +0100

    Updated script for creating storagea containers. Now in NodeJS instead of bash

---
+++ docker/compose/create-storage-containers.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+// deps
+var _ = require('lodash');
+var YAML = require('yamljs');
+var async = require('async');
+var exec = require('child_process').exec;
+
+// load file
+var MAPIC_DOMAIN = process.env.MAPIC_DOMAIN || 'localhost';
+var yml_file = YAML.load('yml/' + MAPIC_DOMAIN + '.yml');
+
+// store 
+var parsed = {};
+
+// settings
+var paths = {
+        engine : '/data',
+        mile : '/data',
+        redisstats : '/data',
+        redislayers : '/data',
+        redistokens : '/data',
+        mongo : '/data/db',
+        postgis : '/var/lib/postgresql',
+}
+
+// parse
+_.each(yml_file, function (value, key) {
+    if (value.volumes_from) {
+        parsed[key] = value.volumes_from[0];
+    }
+});
+
+// create commands
+var commands = [];
+_.each(parsed, function (name, service) {
+    var docker_command = [
+        'docker', 
+        'create', 
+        '-v', 
+        paths[service],
+        '--name', 
+        name,
+        'mapic/ubuntu'
+    ].join(' ');
+    commands.push(docker_command);
+});
+
+// run ops
+async.eachSeries(commands, function (item, callback) {
+
+    // run docker commands
+    exec(item, function (err, stdout) {
+        callback(null);
+    })
+
+}, function (err, result) {
+    console.log('All containers created for ' + MAPIC_DOMAIN);
+});
+
+
+
