e2e84b09e41f6aecf439bb1c3654a5471772388d
knutole <knutole@mapic.io>
2016-12-07 21:51:52 +0100

    Silence script output

--- docker/compose/create-storage-containers.sh
+++ docker/compose/create-storage-containers.sh
@@ -4,7 +4,7 @@ echo "Preparing to create storage containers..."
 docker run -it --rm --name create-containers -v "$PWD":/usr/src/app -w /usr/src/app node:4 node .storage-script-helper.js
 
 echo "Creating storage containers..."
-sh createcontainers.tmp.sh
+sh createcontainers.tmp.sh 2> /dev/null
 
 # cleanup
 echo "Storage containers created!"
