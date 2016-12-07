190239e9fa96cfc2d7f918f0e4db1baa25974fd3
knutole <knutole@mapic.io>
2016-12-07 20:26:12 +0100

    cleanup; explanation

--- docker/compose/.storage-script-helper.js
+++ docker/compose/.storage-script-helper.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 //
-// Creates a bash script which will create correct storage containers
+// Creates a bash script which will create correct storage containers. Can't create storage containers from this script, 
+// since it's run within a Docker container. Containers must be created on host.
+// Therefore the cleanup must also happen in `create-storage-containers.sh`.
 //
-
 
 // install node modules
 var exec = require('child_process').exec;
 var child = exec('npm install lodash yamljs async', function (err, stdout, stderr) {
-    if (err) console.log('exec error: ' + err);
+    if (err) console.log('Creating storage containers failed! [exec error:', err, ']');
 
     // deps
     var _ = require('lodash');
     var YAML = require('yamljs');
     var async = require('async');
-    var exec = require('child_process').exec;
     var fs = require('fs');
 
     // load file
@@ -30,8 +30,8 @@ var child = exec('npm install lodash yamljs async', function (err, stdout, stder
         redisstats  : '/data',
         redislayers : '/data',
         redistokens : '/data',
-        mongo       : '/data/db',
-        postgis     : '/var/lib/postgresql',
+        mongo       : '/data/db',           // todo: simplify this path to /data/ like the rest
+        postgis     : '/var/lib/postgresql', // todo: simplify this path to /data/ like the rest
     }
 
     // parse
--- docker/compose/create-storage-containers.sh
+++ docker/compose/create-storage-containers.sh
@@ -4,10 +4,9 @@ echo "Preparing to create storage containers..."
 docker run -it --rm --name create-containers -v "$PWD":/usr/src/app -w /usr/src/app node:4 node .storage-script-helper.js
 
 echo "Creating storage containers..."
-# sh createcontainers.tmp.sh &>/dev/null
 sh createcontainers.tmp.sh
 
-# clenup
+# cleanup
 echo "Storage containers created!"
 rm -r node_modules
 rm createcontainers.tmp.sh
