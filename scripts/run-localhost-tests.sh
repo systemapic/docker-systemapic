7da42263843e4958df85cfa3c63bf5ef07f0aeac
Terrence Lam <skyuplam@gmail.com>
2016-12-08 09:24:25 +0100

    Minor changes

--- scripts/run-localhost-tests.sh
+++ scripts/run-localhost-tests.sh
@@ -3,4 +3,4 @@
 # localhost tests
 docker exec -it localhost_mile_1 npm test
 docker exec -it localhost_engine_1 npm test
-docker exec -it localhost_engine_1 bash public/test/test.sh
+docker exec -it localhost_engine_1 npm test
