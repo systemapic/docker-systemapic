b9681d57ad29dd207be946fbaa52450a8aab55b5
knutole <knutole@mapic.io>
2016-12-08 16:27:22 +0100

    Cleanup MAPIC_DOMAIN env

--- docker/compose/shell-to.sh
+++ docker/compose/shell-to.sh
@@ -8,10 +8,10 @@ function abort() {
 test -n "$1" || abort "Usage: $0 <name>"
 NAME=$1
 
-# check SYSTEMAPIC_DOMAIN is set
-test -z "$SYSTEMAPIC_DOMAIN" &&
-  abort "Usage: $0 <domain> (or set SYSTEMAPIC_DOMAIN ENV variable, eg. export SYSTEMAPIC_DOMAIN=localhost)"
+# check MAPIC_DOMAIN is set
+test -z "$MAPIC_DOMAIN" &&
+  abort "Usage: $0 <domain> (or set MAPIC_DOMAIN ENV variable, eg. export MAPIC_DOMAIN=localhost)"
 
-PREFIX=`echo ${SYSTEMAPIC_DOMAIN} | sed 's/\..*//'`
+PREFIX=`echo ${MAPIC_DOMAIN} | sed 's/\..*//'`
 FULLNAME=${PREFIX}_${NAME}_1
 docker exec -ti ${FULLNAME} bash
