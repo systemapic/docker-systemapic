25182c0d1759a01c4d470de1761713e19a2514c6
knutole <knutole@mapic.io>
2017-05-05 13:06:16 +0200

    Updated Docker install script

--- scripts/install/install-docker.ubuntu.sh
+++ scripts/install/install-docker.ubuntu.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# install docker
+# install docker community edition
+# updated May 5th 2017
+#
+# https://docs.docker.com/engine/installation/linux/ubuntu/
+
+# install docker-ce
+sudo apt-get remove -y docker docker-engine
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update -y
-sudo apt-get install apt-transport-https ca-certificates -y
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update -y
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
-sudo apt-get update -y
-sudo apt-get install docker-engine -y
+sudo apt-get install -y docker-ce
+
 
 # install docker-compose
-sudo apt-get install curl -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-# print versions
-docker --version
-docker-compose --version
+# verify version
+clear
+echo "Installed Docker Community Edition and Docker Compose:"
+docker version
+docker-compose version
