5419017b33d9b94ab1d5718c0fcbb64d4e06ed2c
knutole <knutole@mapic.io>
2017-05-05 16:50:31 +0200

    Updated to xenial + Node 6.10.3

--- docker/build/base-images/ubuntu/install-nodejs.sh
+++ docker/build/base-images/ubuntu/install-nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # NodeJS version
-NODE_VERSION=6.9.5
+NODE_VERSION=6.10.3
 
 # install
 gpg --keyserver pool.sks-keyservers.net --recv-keys 7937DFD2AB06298B2293C3187D33FF9D0246406D 114F43EE0176B71C7BC219DD50A3051F888C628D
@@ -10,8 +10,3 @@ curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.
     && rm "node-v$NODE_VERSION-linux-x64.tar.gz" \
     && npm install -g npm \
     && npm cache clear
-
-# # install node6 with package manager
-# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-# sudo apt-get install -y build-essential
