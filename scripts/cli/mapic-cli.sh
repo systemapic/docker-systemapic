03ab4d0194dc32c808594430d290a19285b85346
knutole <knutole@mapic.io>
2017-05-20 10:14:45 +0000

    Added test suite to mapic cli

--- scripts/cli/mapic-cli.sh
+++ scripts/cli/mapic-cli.sh
@@ -4,6 +4,7 @@
 # ----- 
 # 1. Create an ENV.sh file to source each time mapic-cli is run. 
 #    That way we don't have to worry about globals, just our own env file.
+#    Need to clean up this ENV across Mapic
 
 # get absolute path of mapic-cli.sh
 D="$(readlink -f "$0")"
@@ -79,6 +80,20 @@ dns_usage () {
     echo ""
     exit 1   
 }
+test_usage () {
+    echo ""
+    echo "Usage: mapic test [OPTIONS]"
+    echo ""
+    echo "Options:"
+    echo "  all         Run all available Mapic tests"
+    echo "  engine      Run all Mapic Engine tests"
+    echo "  mile        Run all Mapic Mile tests"
+    echo "  mapicjs     Run all Mapic.js tests"
+    echo "  travis      Run a build test on Travis"
+    echo ""
+    exit 1   
+}
+
 
 # mapic-cli functions
 mapic_ps () {
@@ -182,13 +197,52 @@ mapic_ssl_scan () {
 mapic_dns () {
     test -z "$2" && dns_usage
     case "$2" in
-        create)        mapic_dns_set;;
+        create)     mapic_dns_set;;
         *)          dns_usage;
     esac 
 }
 mapic_dns_set () {
     cd $MAPIC_ROOT_FOLDER/scripts/cli
     bash create-dns-entries-route-53.sh
+}
+mapic_status () {
+    cd $MAPIC_ROOT_FOLDER/scripts/cli
+    bash mapic-status.sh
+}
+mapic_test () {
+    test -z "$2" && test_usage
+    case "$2" in
+        all)        mapic_test_all;;
+        engine)     mapic_test_engine;;
+        mile)       mapic_test_mile;;
+        mapicjs)    mapic_test_mapicjs;;
+        travis)     mapic_test_travis;;
+        *)          test_usage;
+    esac 
+}
+mapic_test_all () {
+    echo "testing all"
+    mapic run engine npm test || mapic_test_failed
+}
+mapic_test_engine () {
+    echo "Not yet supported."
+    exit 0;
+}
+mapic_test_mile () {
+    echo "Not yet supported."
+    exit 0;
+}
+mapic_test_travis() {
+    echo "Not yet supported."
+    exit 0;
+}
+mapic_test_mapicjs () {
+    echo "Not yet supported."
+    exit 0;
+}
+mapic_test_failed () {
+    echo "Some tests failed";
+    exit 1;
 }
 
 
@@ -200,19 +254,21 @@ mapic_help () {
     echo "A CLI for Mapic"
     echo ""
     echo "Commands:"
-    echo "  install [domain]    Install Mapic."
-    echo "  start               Start Mapic server"
-    echo "  restart             Stop, flush and start Mapic server"
-    echo "  stop                Stop Mapic server"
-    echo "  enter [filter]      Enter running container. Filter in grep format for finding Docker container."
-    echo "  run [filter] [cmd]  Run command inside a container."
+    echo "  install [domain]    Install Mapic"
+    echo "  start               Start Mapic stack"
+    echo "  restart             Stop, flush and start Mapic stack"
+    echo "  stop                Stop Mapic stack"
+    echo "  status              Display status on running Mapic stack"
     echo "  logs                Show logs of running Mapic server"
     echo "  logs dump           Dump logs of running Mapic server to disk"
+    echo "  enter [filter]      Enter running container (with [filter] in grep format for finding Docker container)"
+    echo "  run [filter] [cmd]  Run command inside a container."
     echo "  user                Show user information"
     echo "  ps                  Show running containers"
     echo "  dns                 Create or check DNS entries for Mapic"
     echo "  ssl                 Create or scan SSL certificates for Mapic"
-    echo "  help                This is it."
+    echo "  test                Run Mapic tests"
+    echo "  help                This is it!"
     echo ""
     exit 0
 }
@@ -231,13 +287,15 @@ case "$1" in
     start)      mapic_start;;
     restart)    mapic_start;;
     stop)       mapic_stop;;
+    status)     mapic_status "$@";;
+    logs)       mapic_logs "$@";;
     enter)      mapic_enter "$@";;
     run)        mapic_run "$@";;
-    logs)       mapic_logs "$@";;
     user)       mapic_user "$@";;
     ps)         mapic_ps;;
     dns)        mapic_dns "$@";;
     ssl)        mapic_ssl "$@";;
+    test)       mapic_test "$@";;
     help)       mapic_help;;
     *)          mapic_help;;
 esac
