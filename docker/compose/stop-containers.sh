d5a96a64627d95df8b508a0ba8077d3981b02270
knutole <knutole@mapic.io>
2017-05-21 11:32:45 +0000

    improved start/stop, cleanup

--- docker/compose/stop-containers.sh
+++ docker/compose/stop-containers.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-function abort() {
-	echo $1
-	exit 1;
-}
-
-# ensure domain env set (default: localhost)
-if [ -z "$MAPIC_DOMAIN" ]; then
-    MAPIC_DOMAIN=localhost
-fi
-export MAPIC_DOMAIN
-
 # get file and name (eg. dev.mapic.io.yml and dev)
 COMPOSEFILE="yml/$MAPIC_DOMAIN".yml
 COMPOSENAME=${MAPIC_DOMAIN//./}
