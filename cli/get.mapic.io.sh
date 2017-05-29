e486f0b8c94da55ffd556410bc2349082013597c
knutole <knutole@mapic.io>
2017-05-30 00:44:03 +0100

    comsetic

--- cli/get.mapic.io.sh
+++ cli/get.mapic.io.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#
+# 
 # This script is meant for quick & easy install via:
-#   'curl -sSL https://get.mapic.io/ | sh'
-# or:
 #   'wget -qO- https://get.mapic.io/ | sh'
+# or:
+#   'curl -sSL https://get.mapic.io/ | sh'
 #
-# The source of this script lives at https://raw.githubusercontent.com/mapic/mapic/master/cli/get.mapic.io.sh
-#
-#
-
+# #
 
 # clone mapic
 git clone https://github.com/mapic/mapic.git
