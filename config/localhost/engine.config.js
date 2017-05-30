9b9dabb5ce84eb4dc37f50e87cbe2d08d0f47504
knutole <knutole@mapic.io>
2017-05-30 19:19:51 +0100

    localhost

--- config/localhost/engine.config.js
+++ config/localhost/engine.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "serverConfig": {
-    "instance": "tx - dev",
+    "instance": "[localhost]",
     "token": {
       "accessTokenLength": "256",
       "refreshTokenLength": "256",
