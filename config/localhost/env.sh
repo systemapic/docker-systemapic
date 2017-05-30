a33c701fedd3d83b420ab85b74bbef7a621397a7
knutole <knutole@mapic.io>
2017-05-30 19:17:23 +0100

    removed config, sdk ffrom modules (properly), and moved localhost config to mapic/mapic repo

---
+++ config/localhost/env.sh
@@ -0,0 +1,10 @@
+# ENV file for systemapic
+
+# Username to use for connecting to the database
+SYSTEMAPIC_PGSQL_USERNAME=systemapic
+
+# Password to use for connecting to the database
+SYSTEMAPIC_PGSQL_PASSWORD=docker
+
+# Reference database (currently not used for much)
+SYSTEMAPIC_PGSQL_DBNAME=systemapic
