c0741689c1a518eab085f84bde84e81739bf3bf1
knutole <knutolesjoli@gmail.com>
2017-05-30 19:06:37 +0200

    updated images; starting move to one repo for each image

--- docker/build/engine/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function abort() {
-	echo $1
-	exit 1;
-}
-
-# build
-docker build -t mapic/engine:latest . || abort "Build failed!"
--- docker/build/mile/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function abort() {
-        echo $1
-        exit 1;
-}
-
-# build
-docker build -t mapic/mile:latest . || abort "Build failed!"
