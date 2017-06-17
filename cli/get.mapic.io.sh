53236bd8b7d8e4b7e408fd54137b17366cf85f81
knutole <knutole@mapic.io>
2017-06-17 17:23:32 +0100

    travis install

--- cli/get.mapic.io.sh
+++ cli/get.mapic.io.sh
@@ -10,8 +10,14 @@
 # todo: ubuntu/osx/windows compatibility.
 # use wget/unzip instead of git?
 
+echo "get.mapic script"
+
 # clone mapic
-git clone git@github.com:mapic/mapic.git
+git clone git@github.com:mapic/mapic.git mapic
+
+ls -l
+
+
 
 # install mapic-cli
 cd mapic/cli && bash mapic-cli.sh
